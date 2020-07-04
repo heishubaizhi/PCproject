44f0b02a39e13cbfd6660a927c5871bdc2b551a6
Huang Baishan <1147277199@.qq.com>
2020-07-04 18:23:50 +0800

    'home菜单栏'

---
+++ myproject/src/views/home/Head.vue
@@ -0,0 +1,50 @@
+<template>
+
+  <div class="head_box">
+    <div class="th_left">
+      <img src="../../assets/img/logo.png" alt="">
+      <h1>电商后台管理系统</h1>
+    </div>
+    <div class="th_right">
+      <el-button type="info">退出</el-button>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "Head"
+  }
+</script>
+
+<style scoped lang="less">
+  .head_box{
+    width: 100%;
+    height: 60px;
+    background: #373D41;
+    display: flex;
+    .th_left{
+      height: 100%;
+      display: flex;
+      flex: 1;
+      img{
+        height: 100%;
+      }
+      h1{
+        height: 100%;
+        display: flex;
+        align-items: center;
+        font-weight: normal;
+        font-size: 20px;
+        color: white;
+      }
+    }
+    .th_right{
+      display: flex;
+      /*flex: 1;*/padding-right: 20px;
+      justify-content: right;
+      align-items: center;
+    }
+  }
+</style>
--- myproject/src/views/home/Home.vue
+++ myproject/src/views/home/Home.vue
@@ -1,13 +1,41 @@
 <template>
-  <h1>这里是home</h1>
+  <div class="home_box">
+    <cHead></cHead>
+    <div class="test_main">
+      <div class="tm_left">
+        <cMenu></cMenu>
+      </div>
+      <div class="tm_right">
+
+      </div>
+    </div>
+
+
+  </div>
 </template>
 
 <script>
+  import cHead from './Head'
+  import cMenu from './Menu'
   export default {
-    name: "Home"
+    name: "Home",
+    components:{
+      cHead,cMenu
+    }
   }
 </script>
 
 <style scoped lang="less">
-
+  .home_box{
+    .test_main{
+      width: 100%;
+      height: calc(100vh - 60px);
+      background: rgba(0,0,0,0.3);
+      .tm_left{
+        width: 200px;
+        height: 100%;
+        background: #333744;
+      }
+    }
+  }
 </style>
